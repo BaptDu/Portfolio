b3ebbc7f6fc07a8224330707ecfa21b23282a1a9
BaptDu <baptiste.dumont@ik.me>
2021-06-23 09:21:27 +0200

    style: :heavy_minus_sign: Deleting auto-generated files

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@portfolio/api-interfaces';
-
 @Injectable()
-export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
-}
+export class AppService {}
